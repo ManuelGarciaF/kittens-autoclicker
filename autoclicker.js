a8d6c4f271d7689768a1ce47c425e5293ba3fba4
Manuel Garcia F <mgarciafrigo@gmail.com>
2020-06-19 00:40:25 -0300

    add loop

--- autoclicker.js
+++ autoclicker.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+
 const checkHuntsLeft = () => Number(
   document
     .getElementById('fastHuntContainerCount')
@@ -9,3 +11,10 @@ const sendHunters = () => document
   .getElementById('fastHuntContainer')
   .firstElementChild
   .click();
+
+console.log('Starting autoclicker loop');
+setInterval(() => {
+  if (checkHuntsLeft > 9) {
+    sendHunters();
+  }
+}, 1000);
