ecf24701d76132fd37af1f6662985ba822358a06
Manuel Garcia F <mgarciafrigo@gmail.com>
2020-06-22 20:10:20 -0300

    add steel crafting and resource querying

--- autoclicker.js
+++ autoclicker.js
@@ -2,6 +2,16 @@
 
 const log = (message) => console.log(`Autoclicker: ${message}.`);
 
+const getResourceStock = (resourceName) => {
+  const query = `.resource_${resourceName}`;
+
+  try {
+    return Number(document.querySelector(query).querySelector('.resAmount').innerText);
+  } catch {
+    return 0;
+  }
+};
+
 const checkHuntsLeft = () => {
   if (document.getElementById('fastHuntContainer').style.visibility === 'hidden') return 0;
 
@@ -18,10 +28,23 @@ const sendHunters = () => document
   .firstElementChild
   .click();
 
-log('Starting autoclicker loop');
+const craftSteel = () => document
+  .querySelector('.resource_steel')
+  .querySelector('.craft-link.all')
+  .click();
+
+// Main Loop
 setInterval(() => {
+  // Hunters
   if (checkHuntsLeft() > 9) {
     sendHunters();
     log('Sending hunters');
   }
+
+  // Steel crafting
+  if (getResourceStock('iron') >= 500 && getResourceStock('coal') >= 500) {
+    craftSteel();
+    log('Crafting steel');
+  }
 }, 1000);
+log('Started autoclicker loop');
